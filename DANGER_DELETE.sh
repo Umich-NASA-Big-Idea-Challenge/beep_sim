9c74659a4aaa1202268362598c8b2131addc6ba1
JustinBoverhof <boverhof@umich.edu>
2025-01-05 21:35:06 -0500

    made small changes

--- DANGER_DELETE.sh
+++ DANGER_DELETE.sh
@@ -4,5 +4,6 @@ docker network rm ros
 docker volume rm nasa
 
 docker rm roscore
+docker image rm osrf/ros:humble-desktop-full
 docker image rm theasp/novnc:latest
 docker image rm ros-gazebo-desktop
